24bfb4841c61ddbaa0e870d8b312a2f31e853dbf
vvamshi6 <vvamshi6@gmail.com>
2016-08-10 18:36:30 +0530

    BasicPrograms

--- AddRowandColumn.js
+++ AddRowandColumn.js
@@ -3,22 +3,28 @@ function addColumn(tblId)
 	var tblHeadObj = document.getElementById(tblId).tHead;
 	for (var h=0; h<tblHeadObj.rows.length; h++) {
 		var newTH = document.createElement('th');
+		var newinput = document.createElement('input');
 		tblHeadObj.rows[h].appendChild(newTH);
-		newTH.innerHTML =   'Column' + (tblHeadObj.rows[h].cells.length )
+		//newTH.innerHTML =   'Column' + (tblHeadObj.rows[h].cells.length )
+		newTH.innerHTML = prompt('Enter Column Name');
 	}
 
 	var tblBodyObj = document.getElementById(tblId).tBodies[0];
 	for (var i=0; i<tblBodyObj.rows.length; i++) {
-		var newCell = tblBodyObj.rows[i].insertCell(-1);
+		var newinput = document.createElement('input');
+		var newCell = tblBodyObj.rows[i].insertCell(-1).appendChild(newinput);
+		//var newCell = tblBodyObj.rows[i].appendChild(newinput);
 	}
 }
 function addRow(){
-	var table = document.getElementById("tblSample");
+	var table = document.getElementById("table");
 	var rowCount = table.rows.length;
-	var colCount = document.getElementById('tblSample').rows[0].cells.length;
+	var colCount = document.getElementById('table').rows[0].cells.length;
     var row = table.insertRow(rowCount);
+		var element = document.createElement("input");
     for(var i = 0; i < colCount ; i++){
-      row.insertCell(i);
+			var newinput = document.createElement('input');
+      row.insertCell(i).appendChild(newinput);
     }
 }
 
